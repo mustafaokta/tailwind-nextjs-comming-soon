aab3deee04301c464df148533ec28f186835b2cc
Mustafa OKTA <57900964+mustafaokta@users.noreply.github.com>
2025-01-12 00:32:18 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import ComingSoonVersion1 from "@/components/ComingSoonVersion1/ComingSoonVersio
 
 export default function Home() {  
   return (    
-      <>       
+      <div
+      className="md:px-6 bg-[#b0ab9f]"
+      >       
         {/* Standard Version */}
         <StandardComingSoon />
-      </>
+      </div>
   );
 }
