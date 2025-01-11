08b10fddc8e22851cb6c88974de6e86297857ca9
Mustafa OKTA <57900964+mustafaokta@users.noreply.github.com>
2025-01-12 01:06:06 +0300

    ok

--- src/components/ComingSoonContent/ComingSoonContent.tsx
+++ src/components/ComingSoonContent/ComingSoonContent.tsx
@@ -5,25 +5,27 @@ function ComingSoonContent() {
   const { description, title } = data;
   return (
     <>
-      <div className="relative flex flex-col  place-items-center">
-        <h2 className="text-center font-heading my-28 text-3xl sm:text-3xl lg:text-4xl leading-[5rem] sm:leading-[7rem] lg:leading-[7rem] font-black	 ">
+      <div className="relative flex flex-col   place-items-center">
+        <h2 className="text-center font-heading mt-28 text-3xl sm:text-3xl lg:text-4xl leading-[5rem] sm:leading-[7rem] lg:leading-[7rem] font-black	 ">
           <span className="bg-clip-text text-transparent text-white">
             {title}
           </span>
-        {/*   <span className="">⏳</span> */}
+          {/*   <span className="">⏳</span> */}
         </h2>
-        <p
-          className="text-2xl md:text-3xl max-w-3xl text-center font-bold font-serif  text-black font-large"
-          dangerouslySetInnerHTML={{
-            __html: "Maximize Your Brand's Visibility",
-          }}
-        ></p>
-               <p
-          className=" text-sm pb-6 max-w-3xl text-center mb-8  px-9 text-black font-large"
-          dangerouslySetInnerHTML={{
-            __html: "Florida's Busiest Streets Await!",
-          }}
-        ></p>
+        <div className="mt-4">
+          <p
+            className="text-2xl md:text-3xl max-w-3xl text-center font-bold font-serif  text-black font-large"
+            dangerouslySetInnerHTML={{
+              __html: "Maximize Your Brand's Visibility",
+            }}
+          ></p>
+          <p
+            className=" text-sm pb-6  max-w-3xl text-center mb-8  px-9 text-black font-large"
+            dangerouslySetInnerHTML={{
+              __html: "Florida's Busiest Streets Await!",
+            }}
+          ></p>
+        </div>
       </div>
     </>
   );
--- src/components/SubscribeForm/SubscribeForm.tsx
+++ src/components/SubscribeForm/SubscribeForm.tsx
@@ -99,7 +99,7 @@ export default function Home() {
         <div className="md:w-1/3"></div>
         <div className="md:w-2/3">
           <button
-            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            className="shadow bg-[#1c757b] hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
             type="button"
             onClick={handleSubmit}
           >
