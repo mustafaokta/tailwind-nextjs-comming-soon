7deb077d8c3b7edd2552971b33fc7d980e8e0cc1
taneralagz <88929199+taneralagz@users.noreply.github.com>
2025-01-13 18:35:49 +0300

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import { ThemeProvider } from './theme-provider'
 
 import Head from 'next/head'
 
-const inter = Inter({ 
+const inter = Inter({
   weight: ['100', '200', '300', '600', '400', '700', '900'],
-  subsets: ['latin'] 
+  subsets: ['latin']
 })
 
 export const metadata = {
@@ -22,13 +22,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />        
+        <link rel="icon" href="/favicon.ico" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CE4X6ZPPRM"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-CE4X6ZPPRM');
+            `,
+          }}
+        />
       </Head>
-      <body className={ `${inter.className} bg-gray-200	min-h-screen dark:bg-[#0d1117] `}
-      >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>          
+      <body className={ `${inter.className} bg-gray-200\tmin-h-screen dark:bg-[#0d1117] `}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
           <main className='main'>{children}</main>
-        </ThemeProvider>          
+        </ThemeProvider>
       </body>
     </html>
   )
